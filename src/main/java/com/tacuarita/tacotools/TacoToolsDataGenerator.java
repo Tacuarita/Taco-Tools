07df43aea98091da17068326759acaf390730754
Tacuarita <francisco.cardoso@tutamail.com>
2025-11-28 12:55:04 +0000

    Initial Commit

---
+++ src/main/java/com/tacuarita/tacotools/TacoToolsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.tacuarita.tacotools;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TacoToolsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
